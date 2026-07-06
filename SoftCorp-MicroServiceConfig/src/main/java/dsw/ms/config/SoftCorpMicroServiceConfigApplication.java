d8a925f23d0ee28cae4bcb188427ae0bbb1aedba
Angel <ange.cordovac@unmsm.edu.pe>
2026-07-05 22:52:07 -0500

    microservicios de infraestructura

---
+++ SoftCorp-MicroServiceConfig/src/main/java/dsw/ms/config/SoftCorpMicroServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package dsw.ms.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftCorpMicroServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftCorpMicroServiceConfigApplication.class, args);
+	}
+
+}
---
+++ SoftCorp-MicroServiceGateway/src/main/java/dsw/ms/gateway/SoftCorpMicroServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package dsw.ms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftCorpMicroServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftCorpMicroServiceGatewayApplication.class, args);
+	}
+
+}
