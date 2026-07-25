e09883446e139015ecbc3e308f9768b5ee005a72
Angel <ange.cordovac@unmsm.edu.pe>
2026-07-25 07:57:24 -0500

    correcion de despliegue

--- SoftCorp-MicroServiceUsuarios/src/main/java/dsw/ms/usuarios/repository/RefreshTokenRepository.java
+++ SoftCorp-MicroServiceUsuarios/src/main/java/dsw/ms/usuarios/repository/RefreshTokenRepository.java
@@ -4,12 +4,10 @@ import dsw.ms.usuarios.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
